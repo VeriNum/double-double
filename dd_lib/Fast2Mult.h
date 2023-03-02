097d8ba84232b8f5d25685ddba3885547e66df50
Ariel <ak2485@cornell.edu>
2023-03-02 13:28:01 -0500

    organize

--- dd_lib/Fast2Mult.h
+++
@@ -1,8 +0,0 @@
-#ifndef FAST2MULT_DOT_H
-#define FAST2MULT_DOT_H
-
-struct dd_struct {double s; double t;};
-
-void Fast2Mult(struct dd_struct *st, double a, double b);
-
-#endif /* FAST2MULT_DOT_H */
