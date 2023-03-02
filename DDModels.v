903cbb9a36b2691e7d7fa2e4aea6c6022397af59
Ariel <ak2485@cornell.edu>
2023-03-02 15:02:17 -0500

    start def algs

--- DDModels.v
+++ DDModels.v
@@ -35,6 +35,19 @@ Definition TwoSumF_sum (a b : ftype t) := fst (TwoSumF a b).
 
 End TwoSumModel.
 
+Section FastTwoSumModel.
+Context {NANS: Nans} {t : type}.
+
+Definition Fast2Sum (a b : ftype t) :=
+let s := BPLUS a b in
+let z := BMINUS s a in 
+let t := BMINUS b z in (s, t).
+
+Definition Fast2Sum_err (a b : ftype t) := snd (Fast2Sum a b).
+Definition Fast2Sum_sum (a b : ftype t) := fst (Fast2Sum a b).
+
+End FastTwoSumModel.
+
 
 Section Fast2MultModel.
 Context {NANS: Nans} {t : type}.
@@ -50,10 +63,94 @@ End Fast2MultModel.
 
 
 Section DWord.
-Context {NANS: Nans} {t : type}.
+Context  {t : type}.
 
-Definition double_word x : ftype t -> ftype t -> Prop :=
-  fun xh xl => x = rounded t (FT2R xh + FT2R xl).
+(** Definition 1.4 *) 
+Definition double_word : ftype t -> ftype t -> Prop :=
+  fun xh xl => FT2R xh = rounded t (FT2R xh + FT2R xl).
 
 End DWord.
+
+
+Section DWops.
+Context {NANS: Nans} {t : type}.
+
+(** Algorithm 4 : Addition of DW number and FP number *)
+Definition DWPlusFP (xh xl y : ftype t) := 
+let (sh, sl) := TwoSumF xh y in
+let v:= BPLUS xl sl in
+let (zh, zl) := Fast2Sum sh v in (zh, zl).
+
+(** Algorithm 5 : Addition of two DW numbers *)
+Definition SloppyDWPlusDW (xh xl yh yl : ftype t) := 
+let (sh, sl) := TwoSumF xh yh in
+let v:= BPLUS xl yl in
+let w:= BPLUS sl v in
+let (zh, zl) := Fast2Sum sh w in (zh, zl).
+
+(** Algorithm 6 : Addition of two DW numbers *)
+Definition AccurateDWPlusDW (xh xl yh yl : ftype t) := 
+let (sh, sl) := TwoSumF xh yh in
+let (th, tl) := TwoSumF xl yl in
+let c:= BPLUS sl th in
+let (vh, vl) := Fast2Sum sh c in 
+let w:= BPLUS tl vl in
+let (zh, zl) := Fast2Sum vh w in (zh, zl).
+
+(** Algorithm 7 : Multiplication of a DW number by a FP number *)
+Definition DWTimesFP1 (xh xl y : ftype t) := 
+let (ch, cl1) := Fast2Mult xh y in
+let cl2 := BMULT xl y in
+let (th, tl1) := Fast2Sum ch cl2 in 
+let tl2 := BMULT tl1 cl1 in
+let (zh, zl) := Fast2Sum th tl2 in (zh, zl).
+
+(** Algorithm 8 : Multiplication of a DW number by a FP number *)
+Definition DWTimesFP2 (xh xl y : ftype t) := 
+let (ch, cl1) := Fast2Mult xh y in
+let cl2 := BMULT xl y in
+let cl3 := BPLUS cl1 cl2 in
+let (zh, zl) := Fast2Sum ch cl3 in (zh, zl).
+
+(** Algorithm 9 : Multiplication of a DW number by a FP number*)
+Definition DWTimesFP3 (xh xl y : ftype t) := 
+let (ch, cl1) := Fast2Mult xh y in
+let cl3 := BFMA xl y cl1 in
+let (zh, zl) := Fast2Sum ch cl3 in (zh, zl).
+
+(** Algorithm 10 : Multiplication of a DW number by a DW number*)
+Definition DWTimesDW1 (xh xl yh yl : ftype t) := 
+let (ch, cl1) := Fast2Mult xh yh in
+let tl1 := BMULT xh yl in
+let tl2 := BMULT xl yh in
+let cl2 := BPLUS tl1 tl2 in
+let cl3 := BPLUS cl1 cl2 in 
+let (zh, zl) := Fast2Sum ch cl3 in (zh, zl).
+
+(** Algorithm 11 : Multiplication of a DW number by a DW number*)
+Definition DWTimesDW2 := True.
+
+
+(** Algorithm 12 : Multiplication of a DW number by a DW number *)
+Definition DWTimesDW3 := True.
+
+(** Algorithm 13 : Division of a DW number by a FP number*)
+Definition DWDivFP1 := True.
+
+(** Algorithm 14 : Division of a DW number by a FP number*)
+Definition DWDivFP2 := True.
+
+(** Algorithm 15 : Division of a DW number by a FP number*)
+Definition DWDivFP3 := True.
+
+(** Algorithm 16 : Division of a DW number by a DW number*)
+Definition DWDivDW1 := True.
+
+(** Algorithm 17 : Division of a DW number by a DW number*)
+Definition DWDivDW2 := True.
+
+(** Algorithm 18 : Division of a DW number by a DW number*)
+Definition DWDivDW3 := True.
+
+End DWops.
 
