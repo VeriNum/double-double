7db081c0bd97f7b0232a4eac645eb82ad53b76de
Ariel <ak2485@cornell.edu>
2023-05-01 14:29:06 -0400

    remove old

--- dd_lib/AddDWFP.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include "DWArith.h"
-
-void SDWPFP(dword *st, dword *x, float y) {
-   double v;
-   st     = TwoSum(x.s,y);
-   v      = x.t + st.t;
-   st     = Fast2Sum(st.s, v);
-}
