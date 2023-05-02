56f84952fddb0e0b2952ce36298c5d5e458248bd
ak2485 <ak2485@cornell.edu>
2023-05-01 21:12:51 -0400

    clean for best practice

--- dd_lib/DWPFP.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include "DWArith.h"
-
-void SDWPFP(dword *st, dword *x, float y) {
-   double v; dword sh;
-   TwoSum(&sh,x->s,y);
-   v      = x->t + sh.t;
-   Fast2Sum(st,sh.s,v);
-}
