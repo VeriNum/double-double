672e76d7a2ae3ad55f2b80628273217655a096db
ak2485 <ak2485@cornell.edu>
2023-05-01 21:15:26 -0400

    clean for best practice

---
+++ dd_lib/two_sum_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include "dw_arith.h"
+
+int main(void) {
+    struct dword st = {0.0,0.0};
+    double a = 1.0;
+    double b = -1 * pow(2,-3*54);
+    TwoSum (&st, a, b);
+    printf("%1.16g\n",st.s);
+    printf("%1.16g\n",st.t);
+    return 0;
+}
