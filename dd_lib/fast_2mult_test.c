e06e66a0a145e23502ec49d1b2ca2cf307da681d
ak2485 <ak2485@cornell.edu>
2023-05-01 21:16:45 -0400

    clean for best practice

--- dd_lib/fast_2mult_test.c
+++ dd_lib/fast_2mult_test.c
@@ -6,7 +6,7 @@ int main(void) {
     struct dword st = {0.0,0.0};
     double a = 1.0;
     double b = -1.0;
-    Fast2Mult(&st, a, b);
+    fast_2mult(&st, a, b);
     printf("%1.16g\n",st.s);
     printf("%1.16g\n",st.t);
     return 0;
--- dd_lib/two_sum_test.c
+++ dd_lib/two_sum_test.c
@@ -6,7 +6,7 @@ int main(void) {
     struct dword st = {0.0,0.0};
     double a = 1.0;
     double b = -1 * pow(2,-3*54);
-    TwoSum (&st, a, b);
+    two_sum(&st, a, b);
     printf("%1.16g\n",st.s);
     printf("%1.16g\n",st.t);
     return 0;
