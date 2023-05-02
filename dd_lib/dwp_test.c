ba319fae4999d1646889feaac20b071c8a4afc66
ak2485 <ak2485@cornell.edu>
2023-05-01 21:13:25 -0400

    clean for best practice

--- dd_lib/dwp_test.c
+++ dd_lib/dwp_test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-#include "DWArith.h"
+#include "dw_arith.h"
 
 int main(void) {
-    dword st = {0.0,0.0};
-    dword x  = {1.0,0.0};
+    struct dword st = {0.0,0.0};
+    struct dword x  = {1.0,0.0};
     float a = 1.0;
-    SDWPFP (&st, &x, a);
+    dw_plus_sfp(&st, &x, a);
     printf("%1.16g\n",st.s);
     printf("%1.16g\n",st.t);
     return 0;
