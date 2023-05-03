bd2dd5fa30fe9f2dfb7f4ec08473b452c41e5c6a
Ariel Kellison <ak2485@cornell.edu>
2023-05-03 17:29:29 -0400

    rename

--- dd_lib/dw_p_fp.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include "dw_arith.h"
-
-void dw_plus_sfp(struct dword *st,struct dword *x, float y) {
-   double v; struct dword sh;
-   two_sum(&sh,x->s,y);
-   v      = x->t + sh.t;
-   fast_2sum(st,sh.s,v);
-}
