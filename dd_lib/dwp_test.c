e52e1376b0b7d3bcffa7420add1a06182f32ad9e
Ariel Kellison <ak2485@cornell.edu>
2023-05-03 21:58:17 -0400

    dw_plus_fp verif complete

--- dd_lib/dwp_test.c
+++ dd_lib/dwp_test.c
@@ -5,8 +5,8 @@
 int main(void) {
     struct dword st = {0.0,0.0};
     struct dword x  = {1.0,0.0};
-    float a = 1.0;
-    dw_plus_sfp(&st, &x, a);
+    double a = 1.0;
+    dw_plus_fp(&st,&x, a);
     printf("%1.16g\n",st.s);
     printf("%1.16g\n",st.t);
     return 0;
