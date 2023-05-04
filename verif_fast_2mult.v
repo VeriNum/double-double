46508a9fd82d951155a47e915e59e610da2324da
Ariel Kellison <ak2485@cornell.edu>
2023-05-03 21:58:58 -0400

    update naming

--- verif_fast_2mult.v
+++ verif_fast_2mult.v
@@ -14,7 +14,7 @@ Set Bullet Behavior "Strict Subproofs".
 
 Open Scope logic.
 
-Definition st := Tstruct _dword noattr.
+Definition dword := Tstruct _dword noattr.
 
 Definition f2val (pq: ftype Tdouble * ftype Tdouble) : val*val :=
  (Vfloat (fst pq), Vfloat (snd pq)).
@@ -22,14 +22,14 @@ Definition f2val (pq: ftype Tdouble * ftype Tdouble) : val*val :=
 Definition fast_2mult_spec := 
   DECLARE _fast_2mult
   WITH s: val, a : ftype Tdouble, b : ftype Tdouble
-  PRE [ tptr st, tdouble, tdouble ] (* c lang types *)
+  PRE [ tptr dword, tdouble, tdouble ] (* c lang types *)
     PROP()
-    PARAMS (s; Vfloat a; Vfloat b)
-    SEP(data_at_ Tsh st s) (* preds on mem *)
+    PARAMS (s; Vfloat a; Vfloat b) (*functions params *)
+    SEP(data_at_ Tsh dword s) (* preds on mem *)
   POST [ tvoid ]
     PROP()
     RETURN()
-    SEP(data_at Tsh st (f2val (Fast2Mult a b)) s).
+    SEP(data_at Tsh dword (f2val (Fast2Mult a b)) s).
 
 (* Collect the function-API specs together into Gprog: list funspec *)
 Definition Gprog : funspecs := MathASI ++ [fast_2mult_spec].
