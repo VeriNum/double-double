85192959ba8b1c4abbc33cddf58faceb5045fc53
Ariel <ak2485@cornell.edu>
2023-05-04 16:20:08 -0400

    add dw mult dw

---
+++ dd_lib/dw_mult_dw.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include "dw_arith.h"
+
+void dw_mult_dw12(struct dword *zh, struct dword *x, struct dword *y) {
+   double t0; double t1; double c2; double c3; 
+   struct dword ch;
+   fast_2mult(&ch,x->s,y->s);
+   t0     = x->t * y->t;
+   t1	  = fma(x->s,y->t,t0);
+   c2     = fma(x->t,y->s,t1);
+   fast_2sum(zh,ch.s,c3);
+}
+
